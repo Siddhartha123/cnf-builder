f61b4fdb57c0d98c4c4643812bf6cbd2e23f0148
Ippili Sidhartha Kumar <iskumar789@gmail.com>
2021-04-28 15:10:01 +0530

    replaced brackets with newline
    This was done to make the CNF string compatible with mini-sat solver

--- cnf_builder.cpp
+++ cnf_builder.cpp
@@ -6,18 +6,18 @@ int signal::var_cnt = 0;
 
 signal::signal() {
     var = ++var_cnt;
-    cnf = "";
+    cnf = "\n";
 }
 
 void signal::set_zero() {
     ostringstream ss;
-    ss << cnf << "(" << -var << ")";
+    ss << cnf << "\n" << -var;
     cnf = ss.str();
 }
 
 void signal::set_one() {
     ostringstream ss;
-    ss << cnf << "(" << var << ")";
+    ss << cnf << "\n" << var;
     cnf = ss.str();
 }
 
@@ -26,7 +26,7 @@ signal signal::operator~() {
     int a = var;
     int y = v.var;
     ostringstream ss;
-    ss << cnf << "(" << y << " " << a << ")(" << -y << " " << -a << ")";
+    ss << cnf << "\n" << y << " " << a << "\n" << -y << " " << -a;
     v.cnf = ss.str();
     return v;
 }
@@ -37,8 +37,10 @@ signal signal::operator|(signal signal_b) {
     int b = signal_b.var;
     int y = v.var;
     ostringstream ss;
-    ss << cnf << signal_b.cnf << "(" << -y << " " << a << " " << b << ")(" << y << " " << -a << ")("
-       << y << " " << -b << ")";
+    ss << cnf << signal_b.cnf << "\n"
+       << -y << " " << a << " " << b << "\n"
+       << y << " " << -a << "\n"
+       << y << " " << -b;
     v.cnf = ss.str();
     return v;
 }
@@ -49,8 +51,10 @@ signal signal::operator&(signal signal_b) {
     int b = signal_b.var;
     int y = v.var;
     ostringstream ss;
-    ss << cnf << signal_b.cnf << "(" << y << " " << -a << " " << -b << ")(" << -y << " " << a
-       << ")(" << -y << " " << b << ")";
+    ss << cnf << signal_b.cnf << "\n"
+       << y << " " << -a << " " << -b << "\n"
+       << -y << " " << a << "\n"
+       << -y << " " << b;
     v.cnf = ss.str();
     return v;
 }
@@ -61,8 +65,11 @@ signal signal::operator^(signal signal_b) {
     int b = signal_b.var;
     int y = v.var;
     ostringstream ss;
-    ss << cnf << signal_b.cnf << "(" << y << " " << a << " " << -b << ")(" << y << " " << -a << " "
-       << b << ")(" << -y << " " << a << " " << b << ")(" << -y << " " << -a << " " << -b << ")";
+    ss << cnf << signal_b.cnf << "\n"
+       << y << " " << a << " " << -b << "\n"
+       << y << " " << -a << " " << b << "\n"
+       << -y << " " << a << " " << b << "\n"
+       << -y << " " << -a << " " << -b;
     v.cnf = ss.str();
     return v;
 }
@@ -70,3 +77,9 @@ signal signal::operator^(signal signal_b) {
 void signal::print() {
     // print the cnf array as PoS string
 }
+
+// void sat_solve(){
+//     // write the cnf to <var>.cnf
+//     // run minisat_static
+//     // read <var>.out and parse results
+// }
